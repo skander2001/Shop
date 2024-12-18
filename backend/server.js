b18930f3ac712b828d1f307af52b6ee6e216db91
Skander <skandergharbi.contact@gmail.com>
2024-12-18 17:09:40 +0100

    Backend ✅

--- backend/server.js
+++ backend/server.js
@@ -1,69 +1,18 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import {
-    connectDB
-} from './config/db.js';
-import Product from './models/product.model.js';
+import {connectDB} from './config/db.js';
+import productRoutes from './routes/product.route.js'
 
 dotenv.config();
 
-const app = express()
+const app = express();
+const PORT = process.env.PORT || 5001;
 
-app.use(express.json())// to parse json data
+app.use(express.json()) // to parse json data
 
-app.get ("/api/products",async (req,res) => {
-    try {
-        const products = await Product.find({});
-        res.status(200).json({
-            success:true,
-            data:products
-        })
-    } catch (error) {
-        res.status(500).json({
-            success:false,
-            message:"Internal Server Error"
-        })
-    }})
+app.use("/api/products", productRoutes)
 
-app.post("/api/products",async (req, res) => {
-    const product = req.body;
-    if (!product.name || !product.price || !product.image) {
-        return res.status(400).json({
-            success: false,
-            message: "Please enter all fields"
-        });
-    }
-    const newProduct = new Product(product)
-    try {
-        await newProduct.save();
-        res.status(201).json({
-            success: true,
-            data: newProduct
-        })
-    } catch (error) {
-        console.error("Error in Create product: ", error.message);
-        res.status(500).json({
-            success: false,
-            message: "Internal Server Error"
-        })
-    }
-})
-
-
-app.delete("/api/products/:id",async(req,res) =>{
-    const {id} = req.params
-    try {
-        await Product.findByIdAndDelete(id);
-        res.status(200).json({success:true,message:"Product deleted"})
-    } catch (error) {
-        res.status(404).json({success:false,message:"Product not found"})
-    }
-    
-})
-
-
-
-app.listen(5001, () => {
+app.listen(PORT, () => {
     connectDB();
     console.log("Server started at http://localhost:5001");
 })
